6f8b99c17f3eec05f04376efb119d82067c53435
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2026-08-04 12:41:47 +0900

    chore: upgrade to .NET 10 and add Nix flake dev shell

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Toro";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.dotnet-sdk_10
+            ];
+
+            DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+            DOTNET_NOLOGO = "1";
+          };
+        };
+    };
+}
